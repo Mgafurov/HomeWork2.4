e628eee294e2258893f3f10dd81fdacb92d6371e
Malik <poseydon100500@gmail.com>
2023-07-09 16:35:22 +0500

    Добавьте файлы проекта.

---
+++ HomeWork2.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class HomeWork
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите переменные a , b , c , d : ");
+        float a = float.Parse(Console.ReadLine());
+        float b = float.Parse(Console.ReadLine());
+        float c = float.Parse(Console.ReadLine());
+        float d = float.Parse(Console.ReadLine());
+        float result = (a * b - c) - (c * d);
+        Console.WriteLine("Резульатат : " + result);
+        Console.ReadKey();
+    }
+}
